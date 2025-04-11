c2d4f1b71083bed4872622c52ef0cd33b14aa4a7
Le <hdl5108@e1-055710.science.psu.edu>
2025-04-11 11:27:45 -0400

    roh plots

--- scripts/figures/ROH_figures.R
+++ scripts/figures/ROH_figures.R
@@ -16,3 +16,23 @@ roh_summary$pop <- NULL
 #create new data frame
 nroh_sroh <- roh_summary[, c("samples", "nTOTAL", "TOTAL")]
 
+#Create a Scatter plot 
+library(ggplot2)
+
+ggplot(nroh_sroh, aes(x = TOTAL / 1000000, y = nTOTAL, color = group)) +  # Scale X by 1 million
+  geom_point(size = 4) +
+  geom_abline(slope = 1, intercept = 0, linetype = "solid", color = "black") +  # Add x = y line
+  labs(title = "ROH",
+       x = "TOTAL (in millions)",  # Adjust X-axis label to reflect scale
+       y = "nTOTAL (hundreds)",  # Y-axis stays the same
+       color = "Group") +
+  scale_color_manual(values = c("BBIP" = "#817fc7", "SBJO" = "#fe829b", "SJVE" = "#40e0d0")) +  # Custom colors
+  theme_light()
+
+
+
+
+  
+
+
+
